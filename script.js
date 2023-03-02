e13b8425e19918463687b156d95121a00d796ddf
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-03-02 22:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const infiList = document.getElementById("infi-list");
-const loadMoreThreshold = 200; // distance from bottom of page to trigger loading more items
+const loadMoreThreshold = 200; 
 let currentPage = 1;
 
-// Add 10 list items by default
+
 for (let i = 1; i <= 10; i++) {
   const listItem = document.createElement("li");
   listItem.textContent = `Item ${i}`;
   infiList.appendChild(listItem);
 }
 
-// Add 2 more list items when the user scrolls to the bottom of the page
 window.addEventListener("scroll", () => {
   const distanceToBottom = document.documentElement.scrollHeight - (window.innerHeight + window.scrollY);
   if (distanceToBottom < loadMoreThreshold) {
